9966a60c7e88c66dd2b9f1c11058889bf12c63bb
serega <serega@notan.ua>
2025-02-25 00:06:39 +0000

    login

--- projects/php-one-lib/src/lib/pages/sign-in/sign-in.component.ts
+++ projects/php-one-lib/src/lib/pages/sign-in/sign-in.component.ts
@@ -1,29 +1,34 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {BehaviorSubject} from "rxjs";
 import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
-import {AwfControlModule} from "@awerysoftware/awf-components";
+import {AwfControlModule, AwfValidator} from "@awerysoftware/awf-components";
 import {AsyncPipe} from "@angular/common";
+import {Router} from "@angular/router";
 
 @Component({
-	selector: 'app-sign-in',
-	templateUrl: './sign-in.component.html',
-	styleUrls: ['./sign-in.component.css'],
-	imports: [
-		ReactiveFormsModule,
-		AwfControlModule,
-		AsyncPipe
-	],
-	standalone: true
+    selector: 'app-sign-in',
+    templateUrl: './sign-in.component.html',
+    styleUrls: ['./sign-in.component.css'],
+    imports: [
+        ReactiveFormsModule,
+        AwfControlModule,
+        AsyncPipe
+    ],
+    standalone: true
 })
 export class SignInComponent {
-	public loading$ = new BehaviorSubject<boolean>(false);
-	public loginForm = this._fb.group({
-		email: '',
-		password: ''
-	});
+    private readonly router = inject(Router);
+    public loading$ = new BehaviorSubject<boolean>(false);
+    public loginForm = this._fb.group({
+        email: '',
+        password: ''
+    });
 
-	constructor(private _fb: FormBuilder) {
-	}
+    constructor(private _fb: FormBuilder) {
+    }
 
-	public handleSignIn(){}
+    public handleSignIn() {
+        //For test purposes only
+        this.router.navigate(['/awbs']).then();
+    }
 }
